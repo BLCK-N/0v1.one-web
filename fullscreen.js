2fec7c6df93c1e786ac381573167f845dbde7e60
BLCK-N <92610506+BLCK-N@users.noreply.github.com>
2026-09-08 00:58:44 +0200

    Fullscreen only on first entry

--- fullscreen.js
+++ fullscreen.js
@@ -1,25 +1,16 @@
 (function () {
   "use strict";
 
-  function enterFullscreen(event) {
-    var target = event.currentTarget;
-    var request = document.documentElement.requestFullscreen;
+  var entered = false;
 
-    if (!request) {
+  document.addEventListener("click", function () {
+    if (entered || !document.documentElement.requestFullscreen) {
       return;
     }
 
-    event.preventDefault();
-    request.call(document.documentElement).then(function () {
-      window.setTimeout(function () {
-        window.location.href = target.href;
-      }, 250);
-    }).catch(function () {
-      window.location.href = target.href;
+    entered = true;
+    document.documentElement.requestFullscreen().catch(function () {
+      entered = false;
     });
-  }
-
-  document.querySelectorAll("a[href]").forEach(function (link) {
-    link.addEventListener("click", enterFullscreen);
-  });
+  }, { once: true });
 }());
